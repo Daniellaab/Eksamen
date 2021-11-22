fea38a7979a874182b159d82babe0bab189357d6
Daniellaab <dab.denmark@gmail.com>
2021-11-22 11:07:27 +0100

    Sætter op med express og port

---
+++ server.js
@@ -0,0 +1,8 @@
+// Vi benytter os af Express
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server lytter på http://localhost:${PORT}`);
+});
