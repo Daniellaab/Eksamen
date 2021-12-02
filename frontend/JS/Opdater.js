868173faa59e05af9115f03a01dd03d5d5ed2748
Daniellaab <dab.denmark@gmail.com>
2021-12-02 11:42:30 +0100

    opdateret kode

--- frontend/JS/Opdater.js
+++ frontend/JS/Opdater.js
@@ -1,3 +1,34 @@
+//Opdater brugerens konto
+function updateUser() {
+    var gammelEmail =localStorage.getItem("bruger");
+    gammelEmail = JSON.parse(gammelEmail);
+
+    var email = document.getElementById("nyEmail").value; 
+    var kode = document.getElementById("nyKode").value;
+
+    const updateUser = {email: email, kode: kode, gammelEmail: gammelEmail.email};
+    const update = {email: email, kode: kode};
+
+    fetch("http://localhost:1801/brugere/opdater", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updateUser),
+      })
+      .then((response) => response.json())
+      .then((response) => {
+          if (response) {
+              window.alert("Brugeroplysningerne er hermed blevet opdateret!")
+              localStorage.setItem("bruger", JSON.stringify(bruger));
+          }
+        })
+        .catch(() => {
+          window.alert("Fejlmeddelelse");
+        });
+};
+
+
 // document.addEventListener("DOMContentLoaded", (event) => {
 //     const bruger = localStorage.getItem("bruger");
 //     if (bruger) {
